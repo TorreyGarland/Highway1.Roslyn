a74f454dd8bc86c131ef51da081560ea8b8ab412
crazytmack <crazytmack@TELETRON1A>
2016-08-19 21:06:27 -0400

    gsdfg

---
+++ ClassLibrary1/ClassLibrary1/DependencyObjectGenerator.vb
@@ -0,0 +1,15 @@
+﻿Public Class DependencyObjectGenerator
+
+    Public Sub GenerateDependencyObjects(writer As System.IO.TextWriter)
+        Contracts.Contract.Requires(Of ArgumentNullException)(Not IsNothing(writer), NameOf(writer))
+        Dim dependencyObjects = GetDependencyObjects()
+        If (Not String.IsNullOrWhiteSpace(dependencyObjects)) Then
+            writer.WriteLine(dependencyObjects)
+        End If
+    End Sub
+
+    Public Function GetDependencyObjects() As String
+        GetDependencyObjects = Nothing
+    End Function
+
+End Class
